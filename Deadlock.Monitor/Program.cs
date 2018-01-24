d95e750924a920ffdcbbca5c507ae687d5c4970c
Emre Karahan <e.karahan@jollytur.com>
2018-01-24 13:04:28 +0300

    first commit

---
+++ Deadlock.Monitor/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Threading;
+
+namespace Deadlock.Monitor
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            DeadLock.ExecuteDeadlockAvoidance();
+        }
+    }
+
+
+    public class DeadLock
+    {
+        private static readonly object LockA = new object();
+        private static readonly object LockB = new object();
+
+
+        #region Deallock Avoidance  
+
+        private static void MyWork1()
+        {
+            lock (LockA)
+            {
+                Console.WriteLine("Trying to acquire lock on lockB");
+                // This will try to acquire lock for 5 seconds.  
+                if (System.Threading.Monitor.TryEnter(LockB, 5000))
+                {
+                    try
+                    {
+                        // This block will never be executed.  
+                        Console.WriteLine("In DoWork1 Critical Section.");
+                        // Access some shared resource here.  
+                    }
+                    finally
+                    {
+                        System.Threading.Monitor.Exit(LockB);
+                    }
+                }
+                else
+                {
+                    // Print lock not able to acquire message.  
+                    Console.WriteLine("Unable to acquire lock, exiting MyWork1.");
+                }
+            }
+        }
+
+        private static void MyWork2()
+        {
+            lock (LockB)
+            {
+                Console.WriteLine("Trying to acquire lock on lockA");
+                lock (LockA)
+                {
+                    Console.WriteLine("In MyWork2 Critical Section.");
+                    // Access some shared resource here.  
+                }
+            }
+        }
+
+        public static void ExecuteDeadlockAvoidance()
+        {
+            // Initialize thread with address of DoWork1  
+            Thread thread1 = new Thread(MyWork1);
+            // Initilaize thread with address of DoWork2  
+            Thread thread2 = new Thread(MyWork2);
+            // Start the Threads.  
+            thread1.Start();
+            thread2.Start();
+            thread1.Join();
+            thread2.Join();
+            Console.WriteLine("Done Processing...");
+        }
+
+
+        #endregion
+    }
+}
---
+++ Deadlock/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Threading;
+
+namespace Deadlock
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            DeadLock.ExecuteDeadLockCode();
+        }
+    }
+
+    public class DeadLock
+    {
+        private static readonly object LockA = new object();
+        private static readonly object LockB = new object();
+
+        #region Deadlock Case  
+
+        private static void CompleteWork1()
+        {
+            lock (LockA)
+            {
+                Console.WriteLine("Trying to Acquire lock on lockB");
+                lock (LockB)
+                {
+                    Console.WriteLine("Critical Section of CompleteWork1");
+                    //Access some shared critical section.  
+                }
+            }
+        }
+
+        private static void CompleteWork2()
+        {
+            lock (LockB)
+            {
+                Console.WriteLine("Trying to Acquire lock on lockA");
+                lock (LockA)
+                {
+                    Console.WriteLine("Critical Section of CompleteWork2");
+                    //Access some shared critical section.  
+                }
+            }
+        }
+
+        public static void ExecuteDeadLockCode()
+        {
+            Thread thread1 = new Thread(CompleteWork1);
+            Thread thread2 = new Thread(CompleteWork2);
+            thread1.Start();
+            thread2.Start();
+            thread1.Join();
+            thread2.Join();
+            //Below code section will never execute due to deadlock.  
+            Console.WriteLine("Processing Completed....");
+        }
+
+
+        #endregion
+    }
+}
